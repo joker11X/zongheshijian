66cb29de45c9489922f05257c33412c8c3c13600
YSBZJL <142863054+ysbzjl@users.noreply.github.com>
2023-08-26 14:38:04 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,62 @@
+//window.onload = function(){
+//	document.onscroll = function(){
+//		//获取滚动条位置
+//let s1 = document.documentElement.scrollTop;
+//Let s2 = document.body.scrollTop;
+//let scro11 = s1==0?s2:s1;
+////获取视口宽度
+//let width = document.documentElement.clientWidth;
+////获取顶部固定块
+//let search = document.getElementById('fixedBox');
+////判断滚动条超过视口宽度的12%时，搜索块变固定定位
+//if(scroll>width*0.12){
+//search.style.position = 'fixed';
+//search.style.left = '0';
+//search.style.top ='0';
+//   }
+//else{
+//	search.style.position = 'static';
+//}
+// }
+// }
+
+
+//window.onload = function(){
+//	document.onscroll = function(){
+//		//获取滚动条位置
+//		let s1 = document.documentElement.scrollTop;
+//		let s2 = document.body.scrollTop;
+//	let scroll = s1== 0 ? s2 : s1;
+//		//获取视窗宽度
+//		let width = document.documentElement.clientWidth;
+//		//获取固定块
+//		let search = document.getElementById('fixedBox');
+//	//滚动条超过视窗的百分之十二，固定
+//		if(scroll >width *0.12){
+//			search.style.position = 'fixed';
+//			search.style.left = '0';
+//			search.style.top = '0';
+//		}
+//	else{
+//			search.style.position = 'static';
+//		}
+//	}
+//}
+window.onload = function(){
+	document.onscroll = function(){
+		//获取滚动条位置
+		let s1 = document.documentElement.scrollTop;
+		let s2 = document.body.scrollTop;
+		let scroll = s1==0?s2:s1;
+		
+		let width = document.documentElement.clientWidth;
+		let search = document.getElementById('fixedBox');
+		 if(scroll>width*0.12){
+			 search.style.position = 'fixed';
+			 search.style.left = '0';
+			 search.style.top = '0';
+		 }else{
+			 search.style.position = 'static';
+		 }
+	}
+}
