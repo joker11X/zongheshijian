a9d21955e49c3247f9fc99d1b473b972e621f20d
joker11X <90561423+joker11X@users.noreply.github.com>
2023-09-13 11:18:14 +0800

    Delete elm_admin directory

--- elm_admin/src/com/neusoft/elm/dao/AdminDao.java
+++
@@ -1,9 +0,0 @@
-package com.neusoft.elm.dao;
-
-import com.neusoft.elm.po.Admin;
-
-public interface AdminDao {
-    //管理员名称有唯一约束
-    //获取对象用get开头
-    public Admin getAdminByNamePass(String adminName, String password);
-}
